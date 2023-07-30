38decaeae6c00189090ca50bffd82d423a580665
otter00 <ivlevanastia2001@mail.ru>
2023-07-30 12:22:36 +0700

    ts cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +11,6 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
